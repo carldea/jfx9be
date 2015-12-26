753d3520e6e1e3f2f4f20d687408393d6797d46c
Carl Dea <carldea@gmail.com>
2015-12-26 17:11:32 -0500

    Added the code to allow the reader to use parallel streams

--- chap04/AggregateOperations/src/main/java/com/jfxbe/AggregateOperations.java
+++ chap04/AggregateOperations/src/main/java/com/jfxbe/AggregateOperations.java
@@ -1,11 +1,10 @@
 package com.jfxbe;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * Aggregate Operations.
- * @author carldea
+ * @author cdea
  */
 public class AggregateOperations {
 
@@ -14,19 +13,21 @@ public class AggregateOperations {
      */
     public static void main(String[] args) {
         // create a list of values
-        List<Integer> values = Arrays.asList(23, 84, 74, 85, 54, 60);
+        List<Integer> values = Collections.synchronizedList(Arrays.asList(23, 84, 74, 85, 54, 60));
         System.out.println("values: " + values.toString());
 
         // non-local variable to be used in lambda expression.
         int threshold = 54;
         System.out.println("Values greater than " + threshold + " converted to hex:");
+        //java.util.stream.Stream<Integer> stream = values.parallelStream();
         java.util.stream.Stream<Integer> stream = values.stream();
 
         // using aggregate functions filter() and forEach()
-        values.stream()
+        stream
             .filter(val -> val > threshold) // Predicate functional interface
             .sorted()
             .map(dec -> Integer.toHexString(dec).toUpperCase() ) // Consumer functional interface
+            //.forEachOrdered(val -> System.out.println(val)); // for each output values for parallel stream
             .forEach(val -> System.out.println(val)); // for each output values.
     }    
 }
