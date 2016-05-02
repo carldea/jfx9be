cb965efc7f5e91408573b6155375241c4f9d09ec
Carl Dea <carldea@gmail.com>
2016-05-01 21:06:34 -0400

    Updated the create chapter project shell script to be simpler.

--- create_chap_proj.sh
+++ create_chap_proj.sh
@@ -1,40 +1,33 @@
 #####################################################
 # Prompt user to create a chapter and project name
 #####################################################
-
 # Chapter name
 echo "Please enter the chapter name directory (ie. chap01):"
 read chapname
 
-# Project name
-echo "Please enter the project name directory (ie. MyProject):"
-read projname
-
-# Java Main class name
-echo "Please enter the Java class name (ie. MyExample):"
-read javamain
-
-# If chapter directory not already created.
-if [ ! -e $chapname ]; then
-   echo "Creating $chapname "
-   mkdir -p $chapname
-fi
-###### FIX THE PRO
 # Error out if project already exists
-if [ -e $chapname/$projname ]; then
-   echo "Error $chapname/$projname already exists"
+if [ -e $chapname/src ]; then
+   echo "Error $chapname already exists"
    exit 1
 else
-   mkdir -p $chapname/$projname
+   # Java Main class name
+   echo "Please enter a Java class name containing the main method (ie. MyExample) [Simple name]:"
+   read javamain
+
+   mkdir -p $chapname/src
+   mkdir -p $chapname/resources
 
    # Preserve chapX project
-   cp -R chapX/projectX/* $chapname/$projname
+   cp -R chapX/* $chapname
   
-   # Create a project using the java main class.
-   #mv "$chapname/projectX" "$chapname/$projname"
-   sed -i '' "s/ProjectX/$javamain/" "$chapname/$projname/build.gradle"
-   sed -i '' "s/ProjectX/$projname/" "$chapname/$projname/settings.gradle"
-
-   mv "$chapname/$projname/src/main/java/com/jfxbe/ProjectX.java" "$chapname/$projname/src/main/java/com/jfxbe/$javamain.java"
-   sed -i '' "s/ProjectX/$javamain/" "$chapname/$projname/src/main/java/com/jfxbe/$javamain.java"
+   # Create a file using the java main class.
+   mv "$chapname/src/com/jfxbe/ProjectX.java" "$chapname/src/com/jfxbe/$javamain.java"
+   sed -i '' "s/ProjectX/$javamain/" "$chapname/src/com/jfxbe/$javamain.java"
+   
+   # Change the package shell script. Renames the jar file as the chapter name.
+   sed -i '' "s/chapX/$chapname/" "$chapname/package"
+   
+   # Change the README.txt
+   sed -i '' "s/ProjectX/$javamain/" "$chapname/README.txt"
+   sed -i '' "s/chapX/$chapname/" "$chapname/README.txt"
 fi  
